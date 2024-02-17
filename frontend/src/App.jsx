cd2fb004ebf684b9fc0c3386faa3392ab4d7ef39
Tello <tello.m.m.t.95@gmail.com>
2024-02-17 17:16:12 +0300

    ready to deply

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,6 +34,7 @@ export default App
 // git remote add origin https://github.com/moazTello/moazChatApp.git
 // git push -u origin main
 
+// when you make any changes just commit a new commit and run these lines
 // existance repo
 // git remote add origin https://github.com/moazTello/moazChatApp.git
 // git branch -M main
