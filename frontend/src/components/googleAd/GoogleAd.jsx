0ba6405a21de2501db99f5eef96ce35b2134a820
Tello <tello.m.m.t.95@gmail.com>
2024-03-03 00:49:00 +0300

    google ad mine

--- frontend/src/components/googleAd/GoogleAd.jsx
+++ frontend/src/components/googleAd/GoogleAd.jsx
@@ -5,16 +5,33 @@ const AdComponent = () => {
     // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6018328138529620"
     //  crossorigin="anonymous"></script>
     const script = document.createElement('script');
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6018328138529620';
+    // script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6018328138529620';
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456";
     script.async = true;
     script.crossOrigin="anonymous";
+    // script.client = "ca-pub-1234567890123456"
     document.body.appendChild(script);
+
+
+    // <div align="center">
+    // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456" crossorigin="anonymous"></script>
+    // <!-- Homepage Leaderboard -->
+    // <ins class="adsbygoogle"
+    // style="display:inline-block;width:728px;height:90px"
+    // data-ad-client="ca-pub-1234567890123456"
+    // data-ad-slot="1234567890"></ins>
+    // <script>
+    // (adsbygoogle = window.adsbygoogle || []).push({});
+    // </script>
+    // </div>
+
+
 
     window.googletag = window.googletag || { cmd: [] };
     googletag?.cmd?.push(() => {
       // Define an ad slot for div with id "banner-ad".
       googletag
-        ?.defineSlot("/6355419/Travel/Europe/France/Paris", [300, 250], "banner-ad")
+        ?.defineSlot("1234567890", [728, 90], "banner-ad")
         ?.addService(googletag?.pubads());
 
       // Enable the PubAdsService.
@@ -25,7 +42,13 @@ const AdComponent = () => {
     });
   }, []);
 
-  return <div id="banner-ad" style={{marginTop:'5px' }}></div>;
+  // return <div id="banner-ad" style={{marginTop:'5px',width:'728px',height:'90px' }}></div>;
+  return (<ins
+    className="adsbygoogle"
+    style={{ display: 'block', width: '728px', height: '90px' }}
+    data-ad-client="ca-pub-1234567890123456"
+    data-ad-slot="1234567890"
+  ></ins>);
 };
 
 export default AdComponent;
